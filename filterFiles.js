ee7b656e95c1e4933ecdb06e0219064161ea9088
Sigmoid1723 <testwalker9@gmail.com>
2024-10-22 18:59:13 +0530

    clean-up and added header

--- filterFiles.js
+++ filterFiles.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const fs = require('fs');
 
-// Middleware to filter files (only PDFs and directories)
 function filterFiles(directoryToServe) {
   return function (req, res, next) {
     const dirPath = path.join(directoryToServe, req.path);
@@ -9,12 +8,10 @@ function filterFiles(directoryToServe) {
     fs.readdir(dirPath, { withFileTypes: true }, (err, files) => {
       if (err) return next(err);
 
-      // Filter only directories and PDFs (case-insensitive)
       const filteredFiles = files.filter(file => {
         return file.isDirectory() || file.name.toLowerCase().endsWith('.pdf');
       });
 
-      // Save the filtered file list in req._files for later use
       req._files = filteredFiles;
       next();
     });
