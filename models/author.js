0cc9c425518aedbd80cb5e21d9a3c067cfd9c318
Sigmoid1723 <testwalker9@gmail.com>
2025-06-21 19:02:54 +0530

    upload again

---
+++ models/author.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+const Book = require('./book')
+
+const authorSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    }
+})
+
+authorSchema.pre('deleteOne', async function(next) {
+    /* Book.find({ author: this.id },(err,books) => {
+     *     if(err) {
+     *         next(err)
+     *     } else if (books.length > 0) {
+     *         next(new Error('This author has book still'))
+     *     } else {
+     *         next()
+     *     }
+     * }) */
+    try {
+        const query = this.getFilter();
+        const hasBook = await Book.exists({ author: query._id });
+  
+        if (hasBook) {
+            next(new Error("This author still has books."));
+        } else {
+            next();
+        }
+    } catch (err) {
+        next(err);
+    }
+})
+
+module.exports = mongoose.model('Author',authorSchema)
